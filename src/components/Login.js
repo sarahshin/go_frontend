f1ade305584be4c7a30eec096d43c353bf834d6a
Sarah Shin <sarahsujin.shin@gmail.com>
2019-04-17 17:17:42 -0400

    animations added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Redirect, Link } from "react-router-dom";
+import {Animated} from "react-animated-css";
 import smallLogo from './smallgogologo.png'
 
 import { Form, Button, Header, Grid, Message, Image } from 'semantic-ui-react'
@@ -43,40 +44,42 @@ class Login extends React.Component {
         }
       `}
       </style>
-      <Grid textAlign='center' style={{ height: '100%', marginTop: "7em" }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Form size="large" style={{marginTop: "7em"}}>
-            <Image src={smallLogo} avatar/>
-            <Header as="h2" color="yellow">Log In</Header>
-            <Form.Field>
-              <Form.Input
-                fluid icon='user'
-                iconPosition='left'
-                placeholder='E-mail address'
-                onChange={(e) => this.props.handleChange(e)}
-                name="email"
-                value={this.props.email}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-                onChange={(e) => this.props.handleChange(e)}
-                name="password"
-                value={this.props.password}
-              />
-            </Form.Field>
-            <Button color="yellow" fluid size = 'large' type='submit' onClick={this.handleLogin}>Log In</Button>
-          </Form>
-          <div style={{ marginTop: '1.5em'}}>
-            <Message>Don't have an account? <Link to="/signup">Sign up here!</Link></Message>
-          </div>
-        </Grid.Column>
-      </Grid>
+      <Animated animationIn="headShake" isVisible={true}>
+        <Grid textAlign='center' style={{ height: '100%', marginTop: "7em" }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Form size="large" style={{marginTop: "7em"}}>
+              <Image src={smallLogo} avatar/>
+              <Header as="h2" color="yellow">Log In</Header>
+              <Form.Field>
+                <Form.Input
+                  fluid icon='user'
+                  iconPosition='left'
+                  placeholder='E-mail address'
+                  onChange={(e) => this.props.handleChange(e)}
+                  name="email"
+                  value={this.props.email}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                  onChange={(e) => this.props.handleChange(e)}
+                  name="password"
+                  value={this.props.password}
+                />
+              </Form.Field>
+              <Button color="yellow" fluid size = 'large' type='submit' onClick={this.handleLogin}>Log In</Button>
+            </Form>
+            <div style={{ marginTop: '1.5em'}}>
+              <Message>Don't have an account? <Link to="/signup">Sign up here!</Link></Message>
+            </div>
+          </Grid.Column>
+        </Grid>
+      </Animated>
       </div>
     )
   }
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Container, Image } from "semantic-ui-react"
+import {Animated} from "react-animated-css";
 import logo from './gogologo.png'
 
 const MainPage = () => {
   return (
   <div className="">
     <Container text style={{ marginTop: '7em' }}>
-      <Image src={logo} centered />
+    <Animated animationIn="tada" isVisible={true}>
+      <Image src={logo} centered style={{ height: "600px", width: "600px"}}/>
       <i>A travel planning app</i>
+    </Animated>
     </Container>
   </div>
   )
