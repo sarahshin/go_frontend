e39445f9fffd62bc335dd9d993bbe781c53e7ed0
Sarah Shin <sarahsujin.shin@gmail.com>
2019-04-18 10:45:24 -0400

    more updates

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,6 @@
 import React from "react"
 import CalendarDateTime from "./CalendarDateTime"
-import { Item, Button, Image } from 'semantic-ui-react'
+import { Item, Button, Image, Grid, Icon } from 'semantic-ui-react'
 
 
 const Event = ({ tripEvent, removeEvent, handleEventTime, handleEventDate, myTripStartDate, myTripEndDate }) => {
@@ -8,12 +8,41 @@ const Event = ({ tripEvent, removeEvent, handleEventTime, handleEventDate, myTri
   // pass down function for updating event with time & date from App
   // pass down handleChange & onSubmit to CalendarDateTime
 
+  const renderIcon = () => {
+    switch (tripEvent.cat){
+      case "Restaurants":
+        return <Icon floated="right" name="food" size="large" verticalAlign='middle'/>
+        break;
+      case "Arts":
+        return <Icon floated="right" name="paint brush" size="large"verticalAlign='middle' />
+        break;
+      case "Tours":
+        return <Icon floated="right" name="binoculars" size="large" verticalAlign='middle' />
+
+        break;
+      case "Hotels":
+        return <Icon floated="right" name="hotel" size="large" verticalAlign='middle' />
+
+        break;
+      case "Nightlife":
+        return <Icon floated="right" name="bar" size="large" verticalAlign='middle' />
+
+        break;
+      case "Souvenirs":
+        return <Icon floated="right" name="cart" size="large" verticalAlign='middle' />
+        break;
+      default:
+        console.log("how")
+        break;
+    }
+
+  }
+
   return (
     <Item>
       <Image src={tripEvent.imgurl} size="small" floated="left" verticalAlign='middle' />
-      <Item.Content floated="left"verticalAlign="middle" >
+      <Item.Content verticalAlign="middle" >
         <Item.Header><a href={tripEvent.url}>{tripEvent.name}</a></Item.Header>
-        <Item.Meta>{tripEvent.date} | {tripEvent.time}</Item.Meta>
         <Item.Meta>
           <span>{tripEvent.phone}</span>
         </Item.Meta>
@@ -24,15 +53,16 @@ const Event = ({ tripEvent, removeEvent, handleEventTime, handleEventDate, myTri
           <Button basic color='red' onClick={()=> removeEvent(tripEvent)}>Remove</Button>
         </Item.Extra>
       </Item.Content>
-      <CalendarDateTime
-        handleEventDate={handleEventDate}
-        handleEventTime={handleEventTime}
-        myTripStartDate={myTripStartDate}
-        myTripEndDate={myTripEndDate}
-      />
+      {renderIcon()}
     </Item>
 
   )
 }
+// <CalendarDateTime
+// handleEventDate={handleEventDate}
+// handleEventTime={handleEventTime}
+// myTripStartDate={myTripStartDate}
+// myTripEndDate={myTripEndDate}
+// />
 
 export default Event
